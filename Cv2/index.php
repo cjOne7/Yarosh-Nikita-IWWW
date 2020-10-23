48daaa32e2d24cb226bc904544f02ceca761e068
cjOne7 <61429321+cjOne7@users.noreply.github.com>
2020-10-23 23:41:48 +0200

    Remane started page

---
+++ Cv2/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include_once "header.html" ?>
+    <link rel="stylesheet" type="text/css" href="/css/style.css">
+    <title>Main page dedicated games</title>
+</head>
+<body>
+<?php
+include_once "navbar.html";
+?>
+<div style="text-align: center">
+    <h1>To see the content, please, use navbar menu</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores consequuntur, eaque esse laborum modi nam
+        odio possimus provident ratione repellendus suscipit totam vero voluptatibus? Ad autem cum est expedita
+        ipsa.</p>
+</div>
+</body>
+</html>
